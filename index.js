80abf4f92859dfd6b43b549f30a3cf8d3f09eee0
Emilly Agatha <135267138+emyzou@users.noreply.github.com>
2023-09-04 17:24:43 -0300

    index.js

---
+++ index.js
@@ -0,0 +1,82 @@
+/*
+ const filme = {
+    nome: "Coraline",
+    diretor: "Henry Selick",
+    dataDeLançamento: "13 de fevereiro de 2009",
+    elenco: ["Dakota Fanning", "Jennifer Saunders", "Teri Hatcher"],
+    jaAssistido: true
+
+ };
+
+ console.log(filme.nome);
+ console.log(filme.diretor);
+ console.log(filme.dataDeLançamento);
+ console.log(filme["elenco"]);
+ console.log(filme["jaAssistido"]);
+
+filme.personagens =  ["Outra mãe", "Coraline", "Wybie Lovat"];
+console.log(filme.elenco [0] + "->" + filme.personagens[0]);
+console.log(filme.elenco [1] + "->" + filme.personagens[1]);
+console.log(filme.elenco [2] + "->" + filme.personagens[2]);
+filme.elenco[0] = "xuxa";
+console.log(filme.elenco[0]);
+console.log(filme);
+
+
+ const pessoa = {
+    nome: "Emilly",
+    idade: 16,
+    generoMusical: "MPB"
+
+ };
+ 
+ console.log("O nome da pessoa é " + pessoa.nome  "ela tem " + pessoa.idade + "anos e gosta muito de " + pessoa.generoMusical);
+
+
+ const usuario = {
+    nome: 'prof',
+    idade: 25,
+    email: 'prof@senacrs.com.br',
+    cidade: 'são paulo'
+ }
+ 
+console.log(usuario);
+
+const novoUsuario = {
+    ...usuario,
+    nome: 'Joao',
+    idade:28
+}
+ console.log(novoUsuario);
+
+ const listaDeNomes = ["mika", "paula", "vitor"];
+ const copiaListaDeNomes = [...listaDeNomes];
+ console.log(copiaListaDeNomes);
+
+
+
+
+  function informaçoes (info) {
+     const novasInfo = {...info,
+    comidasFav: ["sushi", "arroz"],
+    melhorAmigo: {nome: "Artur",
+    idade: 18}
+     }   
+     
+ 
+     console.log(novasInfo) ;
+     console.log("O nome da pessoa é", novasInfo.nome,  "e suas comidas favoritas são", novasInfo.comidasFav,
+     "Seu melhor amigo se chama", novasInfo.melhorAmigo.nome, "e tem", novasInfo.melhorAmigo.idade);
+     
+ }
+
+const pessoa = {
+    nome: "Emilly",
+    idade: 16,
+    generoMusical: "MPB",
+
+ };
+ informaçoes(pessoa);
+*/
+
+
